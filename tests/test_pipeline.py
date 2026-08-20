87c0e2f3d31b66a1fe31f27e8ed66ee5c81a8287
Chachay <Chachay@users.noreply.github.com>
2026-08-20 10:18:59 -0500

    Update Fourier intersection expectation for R001 story

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,8 +1,12 @@
 from scripts.validate import validate_all
 from scripts.build_intersections import build_intersections
 
-def test_sample_data_valid(): assert validate_all()==[]
+
+def test_sample_data_valid():
+    assert validate_all() == []
+
+
 def test_fourier_is_intersection():
-    rows=build_intersections()
-    hit=[x for x in rows if x['entity']=='concept-fourier-series']
-    assert hit and hit[0]['story_count']==3
+    rows = build_intersections()
+    hit = [x for x in rows if x['entity'] == 'concept-fourier-series']
+    assert hit and hit[0]['story_count'] == 4
